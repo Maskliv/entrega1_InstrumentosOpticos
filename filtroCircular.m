7bda84e9541cbe4e765b19a16c0f2226c3f173d7
maskliv <donovan57ra@gmail.com>
2021-11-03 21:43:44 -0500

    Creado Archivo para la funcion de creacion de filtros circulares

---
+++ filtroCircular.m
@@ -0,0 +1,6 @@
+function [filtroResultado] = filtroCircular(paresDeCentros,radios,filterSize)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
